fede2de2ef8682c73b7b78ecf7176fb2686e4315
blackdevil72 <{ID}+{username}@users.noreply.github.com>
2022-05-09 00:00:06 +0200

    code prep

---
+++ bl_functions.py
@@ -0,0 +1,6 @@
+# funtions for the project go here
+
+def time_passed(date: str) -> int:
+    print(date)
+
+    return date
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#test
+import datetime
+import jinja2
+import json
+import bl_functions
+
+
+data: dict = json.load(open(data.json))
+age: int = time_passed(data['date_of_birth'])
+years_permis_a2: int = time_passed(data['date_permis_a2'])
