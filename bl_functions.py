fff99fe6f62d77c477724c9890d869fa25cdf897
blackdevil72 <{ID}+{username}@users.noreply.github.com>
2022-05-09 10:50:07 +0200

    create time_passed func

--- bl_functions.py
+++ bl_functions.py
@@ -1,6 +1,10 @@
-# funtions for the project go here
+from datetime import date, datetime
 
-def time_passed(date: str) -> int:
-    print(date)
 
-    return date
+def time_passed(input_str):
+    input_date = datetime.strptime(input_str, '%d/%m/%Y')
+    today = date.today()
+    age = today.year - input_date.year - \
+        ((today.month, today.day) < (input_date.month, input_date.day))
+
+    return age
