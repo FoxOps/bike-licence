ee7e91353352ee19757893c029b7efcf731a38e2
blackdevil72 <{ID}+{username}@users.noreply.github.com>
2022-05-18 11:35:29 +0200

    Correct html to dict conversion bugs

--- main.py
+++ main.py
@@ -14,15 +14,20 @@ def bike_licence_info(reponse_formulaire):
             "text": "Les dates ne peuvent être dans le future."
             }
 
-    if age >= 14:
+    if age >= 14 and data.get("permis_b") is False \
+            and data.get("permis_a2") is False:
         permis_list.append(bl_functions.info_permis_am())
 
-    if age >= 16 and data.get("permis_b") is False:
+    if age >= 16 and data.get("permis_b") is False \
+            and data.get("permis_a1") is False \
+            and data.get("permis_a2") is False:
         permis_list.append(bl_functions.info_permis_a1())
-    elif age >= 16 and data.get("permis_b") is True:
+    elif age >= 16 and data.get("permis_b") is True \
+            and data.get("permis_a1") is False \
+            and data.get("permis_a2") is False:
         permis_list.append(bl_functions.info_permis_a1_7heures())
 
-    if age >= 18:
+    if age >= 18 and data.get("permis_a2") is False:
         permis_list.append(bl_functions.info_permis_a2())
     if age >= 20 and data.get("permis_a2") is True and years_permis_a2 >= 2:
         permis_list.append(bl_functions.info_permis_a())
